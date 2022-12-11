36a95e7f1e23ea43c14d7f2b5f9790b63aede21d
Luis Armando Murillo <luisarmando.murillobaltodano@gmail.com>
2022-12-11 14:24:48 -0600

    fix gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,18 @@
-exports.createPages = async function ({ actions }) {
-  fs.readdir('./src/shaders', (_, fileName) => {
-    const key = fileName.replace(/\.ts/g, '')
+const fs = require('fs')
 
-    if (key !== 'index') {
-      actions.createPage({
-        path: `shaders/${key}`,
-        component: require.resolve(`./src/templates/shaders.tsx`),
-        context: { slug: key },
-      })
-    }
+exports.createPages = async function ({ actions }) {
+  fs.readdir('./src/shaders', (_, files) => {
+    console.log(files)
+    files.forEach(fileName => {
+      const key = fileName.replace(/\.ts/g, '')
+  
+      if (key !== 'index') {
+        actions.createPage({
+          path: `shaders/${key}`,
+          component: require.resolve(`./src/templates/shaders.tsx`),
+          context: { slug: key },
+        })
+      }
+    })
   })
 }
